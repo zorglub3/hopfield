00794231992ff755d6081ca427dd69d2fa8df3e6
Thomas Pecseli <pecseli@gmail.com>
2025-02-20 13:54:57 +0000

    PCN first test

--- src/pcn.rs
+++ src/pcn.rs
@@ -91,17 +91,63 @@ impl PCN {
         }
     }
 
-    pub fn inference(&mut self, gamma: f64) {
+    pub fn inference_sensor_step(&mut self, sensor_mask: &[f64], gamma: f64) {
+        let mut ad = self.neurons[0].clone();
+        activation_diff(&mut ad);
+
+        for i in 0..self.neurons[0].len() {
+            if sensor_mask[i].abs() <= f64::EPSILON {
+                self.neurons[0][i] -= gamma * self.errors[0][i];
+            }
+        }
+    }
+
+    pub fn inference_step(&mut self, gamma: f64) {
         for i in 1..self.layers {
             let mut ad = self.neurons[i].clone();
             activation_diff(&mut ad);
 
             let mut et = vec![0.; self.neurons[i].len()];
-            self.weights[i].trans_mul_vec(&self.errors[i - 1], &mut et);
+            self.weights[i - 1].trans_mul_vec(&self.errors[i - 1], &mut et);
 
             for j in 0..self.neurons[i].len() {
                 self.neurons[i][j] += gamma * (-self.errors[i][j] + ad[j] * et[j]);
             }
+        }
+    }
+
+    pub fn inference(
+        &mut self,
+        memory_pattern: &[f64],
+        sensor_pattern: &[f64],
+        gamma: f64,
+        steps: usize,
+    ) {
+        self.memory_mut().copy_from(memory_pattern);
+        self.sensors_mut().copy_from(sensor_pattern);
+
+        for _i in 0..steps {
+            self.prediction(memory_pattern);
+            self.error(sensor_pattern);
+            self.inference_step(gamma);
+        }
+    }
+
+    pub fn inference_with_sensors(
+        &mut self,
+        memory_pattern: &[f64],
+        sensor_pattern: &[f64],
+        gamma: f64,
+        steps: usize,
+    ) {
+        self.memory_mut().copy_from(memory_pattern);
+        self.sensors_mut().copy_from(sensor_pattern);
+
+        for _i in 0..steps {
+            self.prediction(memory_pattern);
+            self.error(sensor_pattern);
+            self.inference_sensor_step(sensor_pattern, gamma);
+            self.inference_step(gamma);
         }
     }
 
@@ -135,6 +181,47 @@ mod test {
 
         for i in 0..8 {
             assert_eq!(pcn.sensors()[i], s[i]);
+        }
+    }
+
+    #[test]
+    fn can_learn_a_pattern() {
+        const SENSORS: usize = 16;
+        const MEMORY: usize = 8;
+
+        let mut pcn = PCN::new(2, MEMORY, SENSORS);
+        let mut s = vec![0.; SENSORS];
+        let mut m = vec![0.; MEMORY];
+
+        for i in 0..SENSORS {
+            s[i] = if i % 2 == 0 { 1. } else { -1. };
+        }
+        for i in 0..MEMORY {
+            m[i] = if i % 2 == 0 { 1. } else { -1. };
+        }
+
+        const T: usize = 1000;
+        const U: usize = 10;
+        const GAMMA: f64 = 0.1;
+        const ALPHA: f64 = 0.1;
+
+        for _i in 0..U {
+            pcn.inference(&m, &s, GAMMA, T);
+            pcn.learn(ALPHA);
+        }
+
+        let mut pattern = vec![0.; SENSORS];
+        for i in 0..(SENSORS / 2) {
+            s[i] = if i % 2 == 0 { 1. } else { -1. };
+        }
+
+        pcn.memory_mut().copy_from(&m);
+        pcn.sensors_mut().copy_from(&pattern);
+
+        pcn.inference_with_sensors(&m, &pattern, GAMMA, T);
+
+        for i in 0..SENSORS {
+            assert!((pcn.sensors()[i] - s[i]).abs() < 0.1);
         }
     }
 }
